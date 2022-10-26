2784f5c66eb94c503922ccdd48135bd521753cc4
Imran189 <imranimu189@gmail.com>
2022-10-26 23:43:31 +0600

    1st commit

--- vue-composion-api/src/views/HomeView.vue
+++ vue-composion-api/src/views/HomeView.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="lol">
+      <button @click="decrement()" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increment()" class="btn">+</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(1);
+
+const increment = () => {
+  counter.value++;
+};
+const decrement = () => {
+  counter.value--;
+};
+</script>
+<style scoped>
+.lol {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn {
+  font-size: 20px;
+  margin: 10px;
+  padding: 5px;
+}
+.counter {
+  font-size: 20px;
+}
+</style>
