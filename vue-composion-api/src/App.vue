778362595b3de5619b47936471476772d72796f8
Imran189 <imranimu189@gmail.com>
2022-10-26 22:23:30 +0600

    1st init

---
+++ vue-composion-api/src/App.vue
@@ -0,0 +1,38 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+  text-decoration: none;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
