116792a40b63a17f6d724c7dfe0cb5264bcc01f2
Imran189 <imranimu189@gmail.com>
2022-10-27 11:18:18 +0600

    global directives

--- vue-composion-api/src/views/HomeView.vue
+++ vue-composion-api/src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
     <div class="lol">
+      <p>{{ counterTitle }}</p>
       <button @click="decrement()" class="btn">-</button>
       <span class="counter">{{ counterData.counter }}</span>
       <button @click="increment()" class="btn">+</button>
       <p>This data is {{ oddOrEven }}</p>
+      <p>Counter Title</p>
+      <input v-model="counterTitle" vAutofocus />
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive, computed } from "vue";
+import { ref, reactive, computed, watch } from "vue";
+import { vAutofocus } from "@/directives/vAutofocus";
 
+const counterTitle = ref("Here is your Counter Title");
 const counterData = reactive({
   counter: 0,
 });
@@ -27,6 +32,15 @@ const oddOrEven = computed(() => {
   if (counterData.counter % 2 === 0) return "even";
   return "odd";
 });
+
+watch(
+  () => counterData.counter,
+  (newCount) => {
+    if (newCount === 5) {
+      alert("Way to go");
+    }
+  }
+);
 </script>
 
 <style scoped>
