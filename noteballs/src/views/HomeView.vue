a5685942db5b141084b166b72fa5eb64dab3cb1c
Imran189 <imranimu189@gmail.com>
2022-10-31 16:51:15 +0600

    pages

--- noteballs/src/views/HomeView.vue
+++ noteballs/src/views/HomeView.vue
@@ -1,28 +1,32 @@
 <script setup></script>
 
 <template>
-  <div class="notes">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">Component</p>
-        <button class="card-header-icon" aria-label="more options">
-          <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </button>
-      </header>
+  <div class="notes py-3">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add s new Note" />
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-3" v-for="i in 3" :key="i">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          iaculis mauris.
-          <a href="#">@bulmaio</a>. <a href="#">#css</a>
-          <a href="#">#responsive</a>
-          <br />
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi,
+          veritatis odio eius sed atque, esse ullam in distinctio assumenda
+          beatae dolore rerum sequi saepe repellat perspiciatis quos optio
+          eveniet facere.
         </div>
       </div>
       <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
         <a href="#" class="card-footer-item">Edit</a>
         <a href="#" class="card-footer-item">Delete</a>
       </footer>
