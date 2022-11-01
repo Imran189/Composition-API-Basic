ec60695d26fe7ade9b40251042e3563d3f554b9f
Imran189 <imranimu189@gmail.com>
2022-11-01 10:31:24 +0600

    reusable component

---
+++ noteballs/src/components/Notes/NoteEditView.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div class="card has-background-success-dark p-4 mb-5">
+    <div class="field">
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Add s new Note"
+          ref="newNoteRef"
+        />
+      </div>
+    </div>
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <button class="button is-link has-background-success">
+          Add New Note
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
